e4f885ee9821dd4d96ac6172f17540ab504ef509
August <hello@aughost.codes>
2020-09-12 17:30:54 -0400

    Cloned to laptop, just testing

--- main.js
+++ main.js
@@ -12,4 +12,7 @@ function Book(title, author, pages, read) {
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'true');
 
+const houseOfLeaves = new Book('House of Leaves', 'Mark Z. Danielewski', '709', 'true');
+
 console.log(theHobbit.info());
+console.log(houseOfLeaves.info());
