06f9bbb63483ea75452789c008a712548e16f6ad
August <hello@aughost.codes>
2020-09-12 19:42:48 -0400

    Modal foundation

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 'use strict';
 
 let library = [];
-const addButton = document.querySelector('.add-book');
 const cardOne = document.querySelector('.card.one');
 const titleOne = document.querySelector('.title.one');
 const authorOne = document.querySelector('.author.one');
 const pagesOne = document.querySelector('.pages.one');
 const haveReadOne = document.querySelector('.have-read.one');
+
+const addButton = document.querySelector('.add-book');
+const modal = document.querySelector('.modal');
+const span = document.querySelector('.close');
+
+addButton.addEventListener('click', function () {
+    modal.style.display = 'block';
+})
+
+span.addEventListener('click', function () {
+    modal.style.display = 'none';
+})
+
+window.addEventListener('click', function (event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+})
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -26,18 +43,18 @@ function displayLibrary() {
 
 }
 
-addButton.addEventListener('click', function () {
-    const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'false');
-    console.log(theHobbit.info());
-    titleOne.textContent = theHobbit.title;
-    authorOne.textContent = theHobbit.author;
-    pagesOne.textContent = theHobbit.pages;
-    if (theHobbit.read === 'true') {
-        haveReadOne.textContent = 'Yes';
-    } else {
-        haveReadOne.textContent = 'No';
-    }
-});
+// addButton.addEventListener('click', function () {
+//     const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'false');
+//     console.log(theHobbit.info());
+//     titleOne.textContent = theHobbit.title;
+//     authorOne.textContent = theHobbit.author;
+//     pagesOne.textContent = theHobbit.pages;
+//     if (theHobbit.read === 'true') {
+//         haveReadOne.textContent = 'Yes';
+//     } else {
+//         haveReadOne.textContent = 'No';
+//     }
+// });
 
 /*
 const houseOfLeaves = new Book('House of Leaves', 'Mark Z. Danielewski', '709', 'true');
