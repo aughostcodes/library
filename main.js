bdd333dbf224043f3b4076105ef6687f4c9d4aad
August <hello@aughost.codes>
2020-09-12 16:03:06 -0400

    Intro exercise

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.info = function() {
-        return `${title} by ${author}, ${pages} pages. `
+        return `${title} by ${author}, ${pages} pages. Read: ${read}.`;
     }
 }
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'true');
+
+console.log(theHobbit.info());
