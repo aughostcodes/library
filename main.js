f119424b85b0aaf0b9e46a3292a1e26a1025bedf
August <hello@aughost.codes>
2020-09-14 02:20:54 -0400

    Comments

--- main.js
+++ main.js
@@ -1,48 +1,59 @@
 'use strict';
 
 let library = [];
+// Mostly for testing -- final solution cannot be hard-coded
 const cardOne = document.querySelector('.card.one');
 const titleOne = document.querySelector('.title.one');
 const authorOne = document.querySelector('.author.one');
 const pagesOne = document.querySelector('.pages.one');
 const haveReadOne = document.querySelector('.have-read.one');
 
+// Grabbing some buttons/areas
 const addButton = document.querySelector('.add-book');
 const modal = document.querySelector('.modal');
 const confirmNew = document.querySelector('.confirm-new-book');
 const span = document.querySelector('.close');
 
+// Grabbing user input values from the modal pop-up
 let newTitleInput = document.querySelector('.new-title-input');
 let newAuthorInput = document.querySelector('.new-author-input');
 let newPagesInput = document.querySelector('.new-pages-input');
 let haveRead = document.querySelector('.have-read');
 
+// Close modal when clicking anywhere outside of modal
 window.addEventListener('click', function (event) {
     if (event.target === modal) {
         closeModal();
     }
 })
 
+// Launch modal on button click
 addButton.addEventListener('click', function () {
     modal.style.display = 'block';
 })
 
+// Close modal using the 'X'
 span.addEventListener('click', closeModal);
 
+// Starts other calls when 'Confirm' button is clicked within modal
 confirmNew.addEventListener('click', addBookToLibrary);
 
+// Generic close modal function
 function closeModal() {
     modal.style.display = 'none';
 }
 
+// The almighty constructor
 function Book(title, author, pages, read) {
     this.title = newTitleInput.value;
     this.author = newAuthorInput.value;
     this.pages = newPagesInput.value;
     this.read = haveRead.value;
-    this.info = function () {
-        return `${title} by ${author}, ${pages} pages. Read: ${read}.`;
-    }
+}
+
+// Prototypal inheritence, so that this function isn't instantiated repeatedly for each book
+Book.prototype.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages. Read: ${this.read}.`;
 }
 
 function addBookToLibrary() {
